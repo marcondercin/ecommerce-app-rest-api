71fb28ad88c9f2992829cb7ac24cc79e4e7e5814
marcondercin <marcondercin@gmail.com>
2022-03-04 11:22:10 -0800

    create tables

---
+++ database.sql
@@ -0,0 +1,31 @@
+CREATE TABLE customers (
+  id integer PRIMARY KEY,
+  first_name varchar(50),
+  last_name varchar(50),
+  email varchar(50),
+  phone varchar(10),
+  postal varchar(6)
+);
+
+CREATE TABLE orders (
+  id integer PRIMARY KEY,
+  customer_id integer REFERENCES customers(id),
+  date_created date
+);
+
+CREATE TABLE products (
+  id integer PRIMARY KEY,
+  name varchar(100),
+  category varchar(100),
+  price money,
+  stock integer
+);
+
+CREATE TABLE orders_products (
+  order_id integer REFERENCES orders(id),
+  product_id integer REFERENCES products(id),
+  quantity integer,
+  price money,
+  PRIMARY KEY (order_id, product_id)
+);
+

6a5333065242b676fcea0d6ca6bc51b5b465bddb
marcondercin <marcondercin@gmail.com>
2022-03-09 15:32:05 -0800

    Add request methods and endpoint paths for CRUD fs

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const db = require('./queries');
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
 });
 
+app.get('/customers', db.getCustomers);
+app.get('/customers/:id', db.getCustomerById);
+app.post('/customers', db.createCustomer);
+app.put('/customers/:id', db.updateCustomer);
+app.delete('/customers/:id', db.deleteCustomer);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
 

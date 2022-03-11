dfc3b8cd75e6285792310adbd57bf2e7ae0cc66f
marcondercin <marcondercin@gmail.com>
2022-03-10 22:05:25 -0800

    figuring out params and queries

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const port = 3000;
 const db = require('./queries');
 
 const usersRouter = require('./users.js');
+const registerRouter = require('./register.js');
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
 });
 
 app.use('/users', usersRouter);
+app.use('/register', registerRouter);
 
 // app.get('/customers', db.getCustomers);
 // app.get('/customers/:id', db.getCustomerById);
--- register.js
+++ register.js
@@ -1,2 +1,13 @@
+const Pool = require('pg').Pool;
+const config = require('./config.js');
+const pool = new Pool(config);
+
 const express = require('express');
 const router = express.Router();
+
+router.post('/', (req, res, next) => {
+  const text = req.query;
+  res.send(text);
+});
+
+module.exports = router;

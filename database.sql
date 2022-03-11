d742fb3ace20d2a5b03253fce956569493c3bd04
marcondercin <marcondercin@gmail.com>
2022-03-11 13:21:15 -0800

    db query schema updates and file location change

--- database.sql
+++ database.sql
@@ -1,21 +1,18 @@
 -- Creating tables
-CREATE TABLE customers (
-  id integer PRIMARY KEY,
-  first_name varchar(50),
-  last_name varchar(50),
-  email varchar(50),
-  phone varchar(10),
-  postal varchar(6)
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username varchar(20),
+  password TEXT
 );
 
 CREATE TABLE orders (
-  id integer PRIMARY KEY,
-  customer_id integer REFERENCES customers(id),
+  id SERIAL PRIMARY KEY,
+  user_id integer REFERENCES users(id),
   date_created date
 );
 
 CREATE TABLE products (
-  id integer PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   name varchar(100),
   category varchar(100),
   price money,
@@ -28,6 +25,11 @@ CREATE TABLE orders_products (
   quantity integer,
   price money,
   PRIMARY KEY (order_id, product_id)
+);
+
+CREATE TABLE cart (
+  id SERIAL PRIMARY KEY,
+  date_modified date
 );
 
 -- Dummy data

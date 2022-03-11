2ebdfdd3525aaf2492fca815122a683d7c21afa4
marcondercin <marcondercin@gmail.com>
2022-03-10 16:29:11 -0800

    router modules

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const db = require('./queries');
+const usersRouter = require('./users.js');
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
 });
 
-app.get('/customers', db.getCustomers);
-app.get('/customers/:id', db.getCustomerById);
-app.post('/customers', db.createCustomer);
-app.put('/customers/:id', db.updateCustomer);
-app.delete('/customers/:id', db.deleteCustomer);
+app.use('/users', usersRouter);
+
+// app.get('/customers', db.getCustomers);
+// app.get('/customers/:id', db.getCustomerById);
+// app.post('/customers', db.createCustomer);
+// app.put('/customers/:id', db.updateCustomer);
+// app.delete('/customers/:id', db.deleteCustomer);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
--- users.js
+++ users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+module.exports = router;
+
 router.get('/', (req, res, next) => {
   res.send('Hello');
 });

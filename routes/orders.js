4839465a21330d66c5eab12eba848860d5de3350
marcondercin <marcondercin@gmail.com>
2022-03-31 09:27:20 -0700

    user id in orders router

--- routes/orders.js
+++ routes/orders.js
@@ -13,3 +13,14 @@ router.get('/', (req, res, next) => {
     res.status(200).json(results.rows);
   });
 });
+
+router.get('/:user_id', (req, res, next) => {
+  const user_id = parseInt(req.params.user_id);
+
+  pool.query('SELECT * FROM orders WHERE user_id = $1', [user_id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+    });
+});
